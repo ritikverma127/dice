6e561d20ed9e5b35ef9fe950b953499744d76763
ritik verma <ritikverma127>
2024-08-10 13:15:46 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import  "./App.css";
 import { useState } from "react";
 import Gameplay from "./Components/Gameplay";
 function App() {
+  const [count, setcount] = useState(0)
   const[gamestarted,setGamestarted]=useState(false);
   const toggleplaygame=()=>{
     setGamestarted((pre)=>!pre);
+  }
+  const Fun = () => {
+    setcount(count+1)
   }
   return (
    <div>
     {
     gamestarted?<Gameplay/>:<Startgame pass={toggleplaygame}/>
     }
-   
+    <h1>{count}</h1>
+   <button onClick={Fun}>Click</button>
     </div>
   );
 }
